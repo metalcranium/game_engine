07367bb2ec5e937c200ead822c6d9ba709f4afbf
blake <metal_cranium07@yahoo.com>
2025-07-03 15:04:56 -0500

    physics work

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #define scr_width 1200
 #define scr_height 800
 #define delta GetFrameTime()
-#define gravity 100
+#define gravity 50
 #define fps 60
 
 class RigidBody2D;
@@ -37,6 +37,23 @@ class RigidBody2D{
       
     }
 };
+class Block : public RigidBody2D{
+  public:
+    float fall;
+
+    Block(){
+      velocity = {0,0};
+      size = {32,32};
+      fall = gravity;
+    }
+    virtual void update(){
+      position.y += velocity.y * fall * delta;
+      velocity.y += 5 * delta;
+    }
+    virtual void draw(){
+      DrawRectangle(position.x, position.y, size.x, size.y, YELLOW);
+    }
+};
 class Player : public RigidBody2D{
   public:
     const float SPEED = 200;
@@ -62,16 +79,18 @@ class Player : public RigidBody2D{
       std::cout << "player destroyed" << std::endl;
     }
 
-    void update(){
-      Vector2Normalize(velocity);
+    virtual void update(){
+      // Vector2Normalize(velocity);
       position.x += velocity.x * speed * delta;
       position.y += velocity.y * fall * delta;
-      std::cout << "is grounded: " << is_grounded << std::endl;
+      velocity.y += 5 * delta;
+
+      // std::cout << "is grounded: " << is_grounded << std::endl;
 
       collider = {position.x, position.y, size.x, size.y};
       input();
     }
-    void draw(){
+    virtual void draw(){
       DrawRectangle(position.x, position.y, size.x, size.y, BLUE);
     }
     void input(){
@@ -85,7 +104,7 @@ class Player : public RigidBody2D{
         velocity.x = 0;
       }
       if (IsKeyPressed(KEY_UP) and can_jump == true){
-        velocity.y = -1;
+        velocity.y = -5;
         is_grounded = false;
       }
       // else if (IsKeyDown(KEY_DOWN)){
@@ -94,7 +113,6 @@ class Player : public RigidBody2D{
       // else{
       //   velocity.y = 0;
       // }
-      velocity.y += 1 * delta;
 
       if (is_grounded){
         can_jump = true;
@@ -115,6 +133,7 @@ int main(){
   // std::unique_ptr<Player> player = std::make_unique<Player>();
   std::shared_ptr<Player>player = std::make_shared<Player>();
   std::vector<Rectangle>boxes;
+  std::vector<std::shared_ptr<Block>>blocks;
   Rectangle collision;
   Rectangle ground = {0, 750, scr_width, 50};
   boxes.push_back(ground);
@@ -128,12 +147,29 @@ int main(){
       *obj = {GetMousePosition().x, GetMousePosition().y, 32, 32};
       boxes.push_back(*obj);
     }
+    if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)){
+      std::shared_ptr<Block>block = std::make_shared<Block>();
+      block->position = {GetMousePosition().x, GetMousePosition().y};
+      block->size = {32,32};
+      blocks.push_back(block);
+    }
     if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
       for (int i = 0; i < boxes.size(); i++){
         if (CheckCollisionPointRec(GetMousePosition(), boxes[i])){
           boxes.erase(boxes.begin()+ i);
         }
       }
+      for (int i = 0; i < blocks.size(); i++){
+        if (CheckCollisionPointRec(GetMousePosition(), {blocks[i]->position.x,
+                                    blocks[i]->position.y, blocks[i]->size.x, blocks[i]->size.y})){
+          blocks.erase(blocks.begin()+ i);
+        }
+      }
+    }
+
+    for (auto i : blocks){
+      i->update();
+      std::cout << i->position.y << std::endl;
     }
     // runs the game from inside the editor in separate window
     if (IsKeyPressed(KEY_F5)){
@@ -153,6 +189,9 @@ int main(){
     for (auto j : boxes){
       DrawRectangleRec(j, RED);
     }
+    for (auto i : blocks){
+      i->draw();
+    }
     DrawRectangleRec(collision, GREEN);
 
     EndDrawing();
@@ -165,27 +204,24 @@ void Resolve_World_Collision(std::shared_ptr<Player>player, std::vector<Rectangl
   Vector2 sign = {0,0};
   bool collided;
   Rectangle collision;
-    // for (Rectangle i : boxes){
-    for (int i = 0; i < boxes.size(); i++){
-      collided = CheckCollisionRecs(player->collider, boxes[i]);
-      collision = GetCollisionRec(player->collider, boxes[i]);
-      if (collided){//(collision.width != 0 and collision.height != 0){
-        sign.x = player->collider.x + player->collider.width < boxes[i].x + boxes[i].width ? 1 : -1;
-        sign.y = player->collider.y + player->collider.height < boxes[i].y + boxes[i].height ? 1 : -1;
-          if (collision.width < collision.height){
-            player->position.x -= collision.width * sign.x;
-          }
-          else if (collision.height < collision.width){
-            std::cout << "collision" << std::endl;
-            player->is_grounded = true;
-            player->position.y -= collision.height * sign.y;
-            player->velocity.y = 0;
-          }
-          else{
-            player->is_grounded = false;
-          }
-
-      }
-
+  for (int i = 0; i < boxes.size(); i++){
+    collided = CheckCollisionRecs(player->collider, boxes[i]);
+    collision = GetCollisionRec(player->collider, boxes[i]);
+    if (collided){//(collision.width != 0 and collision.height != 0){
+      sign.x = player->collider.x + player->collider.width < boxes[i].x + boxes[i].width ? 1 : -1;
+      sign.y = player->collider.y + player->collider.height < boxes[i].y + boxes[i].height ? 1 : -1;
+        if (collision.width < collision.height){
+          player->position.x -= collision.width * sign.x;
+        }
+        else if (collision.height < collision.width){
+          // std::cout << "collision" << std::endl;
+          player->is_grounded = true;
+          player->position.y -= collision.height * sign.y;
+          player->velocity.y = 0;
+        }
+        else{
+          player->is_grounded = false;
+        }
     }
+  }
 }
