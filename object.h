ddecac38fae7970c107e7c90318b95df4a23ec00
metalcranium <metal_cranium07@yahoo.com>
2025-09-01 15:37:06 -0500

    update

--- object.h
+++ object.h
@@ -2,24 +2,24 @@
 #include "raylib.h"
 
 class Object {
-public:
-	Vector2 velocity;
-	Vector2 position;
-	Vector2 size;
-	Rectangle collider;
-	Rectangle source;
-	float speed;
-	float mass;
-	float fall;
-	Texture atlas;
-	Texture texture;
-	bool is_static;
-	bool is_grounded;
+		public:
+			Vector2 velocity;
+			Vector2 position;
+			Vector2 size;
+			Rectangle collider;
+			Rectangle source;
+			float speed;
+			float mass;
+			float fall;
+			Texture atlas;
+			Texture texture;
+			bool is_static;
+			bool is_grounded;
 
-	Object();
-	~Object();
-	virtual void ready();
-	virtual void update();
-	virtual void draw();
+			Object();
+			~Object();
+			virtual void ready();
+			virtual void update();
+			virtual void draw();
 };
 
