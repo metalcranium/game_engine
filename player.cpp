56dac4fa0747005cfd02550c34deb52964e08fe5
metalcranium <metal_cranium07@yahoo.com>
2025-09-08 21:51:01 -0500

    not much

--- player.cpp
+++ player.cpp
@@ -16,6 +16,7 @@ Player::~Player(){
   speed = 200;
   atlas_texture = LoadTexture("Assets/herowalk.png");
   idle_texture = LoadTexture("Assets/hero.png");
+  texture = idle_texture;
   source = {0,0, size.x, size.y};
   animation = new(AnimationPlayer);
 }
@@ -39,10 +40,16 @@ void Player::input(){
   else if (IsKeyDown(KEY_DOWN)){
     velocity.y = 1;
   }
+  else {
+    velocity.y = 0;
+  }
   if (IsKeyDown(KEY_LEFT)){
     velocity.x = -1;
   }
   else if (IsKeyDown(KEY_RIGHT)){
     velocity.x = 1;
+  }
+  else {
+    velocity.x = 0;
   }
 }
