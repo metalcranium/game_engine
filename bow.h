2cdeacffe57f22a68430995dafb8391c31eb681f
metalcranium <metal_cranium07@yahoo.com>
2025-09-17 19:32:36 -0500

    update

---
+++ bow.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "item.h"
+
+class Bow : public Item{
+  public:
+    Bow();
+    ~Bow();  
+};
---
+++ zombie.h
@@ -0,0 +1,9 @@
+#include "raylib.h"
+#include "character.h"
+#include <iostream>
+
+class Zombie : public Character{
+  public:
+    Zombie();
+    ~Zombie();
+};
